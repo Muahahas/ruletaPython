232e7b83b4ce89b325e33754477f3ebfe6229531
Muahahas <eloibuisan@gmail.com>
2015-11-29 15:44:26 +0100

    Canviades cadenes concatenades per el correcte funcionament de gettext

--- ruleta.py
+++ ruleta.py
@@ -16,7 +16,7 @@ def main ():
 	nguanyades=0
 	nperdudes=0
 	print _("Benvingut a la ruleta de la sort.")
-	print _("Tens un capital de " + `capital` +"e, quan vols apostar?")
+	print (_("Tens un capital de " + `capital` +_("e, quan vols apostar?"))
 	aposta =int(raw_input())
 	while (aposta!=0 and capital!=0):
 		#Entrara en aquest bucle mentres s'aposti mes de 0 i es tingui capital.	
@@ -31,19 +31,19 @@ def main ():
 				numero =int(raw_input())
 			#Quan el numero sigui correcte sortira del bucle, fara el sorteig i ens dira si em guanyat o perdut.
 			sorteig = rand.randint(0,37)
-			print _("Has apostat al numero "+`numero`+" i ha sortit el numero " + `sorteig`)
+			print (_("Has apostat al numero ")+`numero`+_(" i ha sortit el numero ") + `sorteig`)
 			if (sorteig==numero):
 				aposta=aposta*36
-				print _("Has guanyat " + `aposta`+ "e.")
+				print (_("Has guanyat ") + `aposta`+ _("e."))
 				capital=capital+aposta
 				nguanyades+=1
 			else:
-				print _("Has perdut "+`aposta`+"e.")
+				print (_("Has perdut ")+`aposta`+_("e.")
 				capital=capital-aposta
 				nperdudes+=1
 			if (capital!=0):
 				#Ens donara l'opcio continuar jugant si el capital no es 0, perque en cas d'arruinar-nos no ens ho demani.
-				print _("Tens un capital "+`capital`+"e, quan vols apostar?")
+				print (_("Tens un capital ")+`capital`+_("e, quan vols apostar?"))
 				aposta = int(raw_input())
 		else:
 			#Si la cantitat apostada era negativa o superior al capital ens dira que ho tornem a provar.
